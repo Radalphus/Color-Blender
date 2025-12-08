b9c0736d31e86982c6d2f6e1f9e642838eb4d83f
Radalphus <128213849+Radalphus@users.noreply.github.com>
2025-12-07 22:04:02 -0800

    Chages to work with github pages

--- color-blender-react/vite.config.ts
+++ color-blender-react/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/color-blender/', // Replace 'color-blender' with your repo name if different
 })
